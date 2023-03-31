3b90b78a7a773e3181c9d841a5120ff1c9853fef
maquenneville <100183934+maquenneville@users.noreply.github.com>
2023-03-30 23:14:55 -0700

    Implemented second phase, improved story writing prompt engineering and added photo generation and pdf creation for final story

--- radventure/BookBuilderHelpers.py
+++ radventure/BookBuilderHelpers.py
@@ -14,6 +14,16 @@ import os
 import configparser
 import sys
 from transformers import GPT2TokenizerFast
+import requests
+from PIL import Image as PIL_Image
+from io import BytesIO
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.units import inch
+
 
 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 
@@ -37,7 +47,7 @@ def get_openai_api_key():
 openai.api_key = get_openai_api_key()
 
 
-def generate_response(messages, temperature=0.5, n=1, max_tokens=4000):
+def generate_response(messages, temperature=0.5, n=1, max_tokens=4000, frequency_penalty=0):
     
     model_engine = "gpt-3.5-turbo"
 
@@ -51,6 +61,9 @@ def generate_response(messages, temperature=0.5, n=1, max_tokens=4000):
     # Reduce max_tokens further if the total tokens exceed the model limit
     if tokens_used + max_tokens > 4096:
         max_tokens = 4096 - tokens_used - 10
+        
+    if max_tokens < 1:
+        max_tokens = 1
 
     # Generate a response
     max_retries = 10
@@ -63,7 +76,8 @@ def generate_response(messages, temperature=0.5, n=1, max_tokens=4000):
                     messages=messages,
                     n=n,
                     temperature=temperature,
-                    max_tokens=max_tokens
+                    max_tokens=max_tokens,
+                    frequency_penalty=frequency_penalty
                 )
                 break
             except (RateLimitError, KeyboardInterrupt):
@@ -79,10 +93,28 @@ def generate_response(messages, temperature=0.5, n=1, max_tokens=4000):
 
 
 
-def build_prompt(genre, target_audience, theme, content_hint, narrator="omniscient"):
-    prompt = f"Write a {genre} children's book targeted at {target_audience} about {theme}, using a {narrator} narrator. {content_hint}"
+def build_prompt(genre, target_audience, theme, setting, content_hint, narrator="omniscient", twist=False):
+    
+    if twist:
+        prompt = f"Write a {genre} children's book targeted at {target_audience} about {theme}.  It takes place in {setting}. Use a {narrator} narrator. Include a surprise twist at some point in the story."
+    
+    else:
+        prompt = f"Write a {genre} children's book targeted at {target_audience} about {theme}.  It takes place in {setting}. Use a {narrator} narrator. {content_hint}"
+    
     return prompt
 
+
+def enhance_build_prompt(prompt):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"I want you to become my Prompt Optimizer. Your goal is to help me craft the best possible prompt for my needs. The prompt will be used by you, ChatGPT.  You will be improving a prompt for creating a children's story.  Your improvements must not change the setting, genre, theme, narrator, characters, or any twists involved.  What improvements can and should be made to this prompt: {prompt}"}
+    ]
+
+    suggestions = generate_response(messages, temperature=0.2, max_tokens=2000)
+    messages.append({"role": "assistant", "content": suggestions})
+    messages.append({"role": "user", "content": "Using your suggestions, improve the prompt for generating content with GPT.  Just generate the revised prompt, no need for explanatory text."})
+    improved_prompt = generate_response(messages, temperature=0.2, max_tokens=2000)
+    return improved_prompt
 
 def enhance_prompt(prompt):
     messages = [
@@ -90,10 +122,10 @@ def enhance_prompt(prompt):
         {"role": "user", "content": f"I want you to become my Prompt Optimizer. Your goal is to help me craft the best possible prompt for my needs. The prompt will be used by you, ChatGPT.  What improvements can and should be made to this prompt: {prompt}"}
     ]
 
-    suggestions = generate_response(messages, temperature=0.2, max_tokens=2000)
+    suggestions = generate_response(messages, temperature=0.2, max_tokens=1000)
     messages.append({"role": "assistant", "content": suggestions})
     messages.append({"role": "user", "content": "Using your suggestions, improve the prompt for generating content with GPT.  Just generate the revised prompt, no need for explanatory text."})
-    improved_prompt = generate_response(messages, temperature=0.2, max_tokens=2000)
+    improved_prompt = generate_response(messages, temperature=0.2, max_tokens=1000)
     return improved_prompt
 
 
@@ -140,7 +172,7 @@ def select_best_outline(enhanced_prompt):
         model=model_engine,
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"You are an incredibly talented children's book author.  You know how to weave a story together in a way that is engaging and fun to read.\nCreate an outline for a children's book based on this prompt: '{enhanced_prompt}'"}
+            {"role": "user", "content": f"You are an incredibly talented children's book author.  You know how to weave a story together in a way that is engaging and fun to read.  You keep things fresh, exciting, and ensure that your stories aren't repetitive.\nCreate an outline for a children's book based on this prompt: '{enhanced_prompt}'"}
         ],
         n=5,
         temperature=1.2,
@@ -188,23 +220,67 @@ def build_first_draft(outline):
     # Iterate through the section prompts and generate content for each section
     for prompt in section_prompts[1:]:
         
-        messages.append({"role": "user", "content": f"Write a section for a children's book based on this part of the outline: '{prompt}'"})
-        section_content = generate_response(messages, max_tokens=2000)
+        messages.append({"role": "user", "content": f"Write a section for a children's book based on this part of the outline, ensuring that the wording is not repetitive: '{prompt}'"})
+        section_content = generate_response(messages, temperature=0.7, max_tokens=2000, frequency_penalty=0.8)
         messages.append({"role": "assistant", "content": section_content})
         sections.append(section_content)
 
     return sections
 
+def check_and_combine_sections(prev_section, cur_section):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant that combines repetitive sections."},
+        {"role": "user", "content": "You are a very talented editor, who takes two sequential sections of a book and check to ensure they are not redundant, and that they're both necessary to the plot of the story. If they aren't, you combine them into one section that is not redundant, is concise, yet maintains the tone, characters and feeling of the section."},
+        {"role": "assistant", "content": "Of course, what sections can I help you with today?"},
+        {"role": "user", "content": f"Previous section: {prev_section}"},
+        {"role": "user", "content": f"Current section: {cur_section}"},
+        {"role": "user", "content": "Are these sections repetitive? If so, make the first word of your response 'Yesindeedy', and then combine them into a single unique section."}
+    ]
+
+    response = generate_response(messages, temperature=0.2, max_tokens=500)
+
+    # Check if the response indicates that the sections are repetitive
+    if "yesindeedy" in response.lower():
+        return response.strip()
+    else:
+        return prev_section, cur_section
 
 
-def continuity_check_prompt(prev_section, curr_section):
-    return f"Does the following section maintain continuity and flow well with the previous section? Respond with only a single word, true or false.  Previous section: '{prev_section}'. Current section: '{curr_section}'"
+def process_story_sections(sections):
+    processed_sections = []
+    prev_section = sections[0]
+
+    for i in range(1, len(sections)):
+        cur_section = sections[i]
+        result = check_and_combine_sections(prev_section, cur_section)
+
+        if isinstance(result, tuple):
+            # If the result is a tuple, add the prev_section to the processed_sections list
+            # and update the prev_section to be the cur_section for the next iteration
+            processed_sections.append(result[0])
+            prev_section = result[1]
+        else:
+            # If the result is a single combined section, update the prev_section to be the combined section
+            prev_section = result
+
+    # Add the final section to the processed_sections list
+    processed_sections.append(prev_section)
+
+    return processed_sections
+
+
+def continuity_check_prompt_first(prev_section, curr_section):
+    return f"Does the following section maintain continuity and flow well with the previous section, and is not repetitive compared to the previous section? Respond with only a single word, true or false.  Previous section: '{prev_section}'. Current section: '{curr_section}'"
+
+def continuity_check_prompt(cur_section):
+    return f"Does the following section maintain continuity and flow well with the previous section? Respond with only a single word, true or false. Current section: '{cur_section}'"
 
 def refine_draft(book_sections, title):
     refined_sections = [book_sections[0]]  # Add the first section as it doesn't need to be checked for continuity
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "system", "content": "You are an extremely talented editor of children's books.  Your specialty is ensuring that the story maintains cohesion, continuity and flows well together.  We're going to ensure that this children's story maintains continuity, step by step."}
+        {"role": "user", "content": "You are an extremely talented editor of children's books.  Your specialty is ensuring that the story maintains cohesion, continuity and flows well together, while not being redundant or repetitive.  We're going to ensure that this children's story maintains continuity, step by step."},
+        {"role": "user", "content": "This is the first section: {refined_sections[0]}."}
     ]
 
     for i in range(1, len(book_sections)):
@@ -212,12 +288,12 @@ def refine_draft(book_sections, title):
         curr_section = book_sections[i]
 
         # Check if the current section maintains continuity with the previous section
-        messages.append({"role": "user", "content": continuity_check_prompt(prev_section, curr_section)})
-        response = generate_response(messages, temperature=0.2, max_tokens=10)
+        messages.append({"role": "user", "content": continuity_check_prompt(curr_section)})
+        response = generate_response(messages, temperature=0.2, max_tokens=5)
 
         while response.lower() not in ('true', 'false', 'true.', 'false.'):
-            messages.append({"role": "user", "content": "Please respond with 'true' or 'false'."})
-            response = generate_response(messages, temperature=0.2, max_tokens=10)
+            messages.append({"role": "user", "content": "Please respond with only one word, 'true' or 'false'."})
+            response = generate_response(messages, temperature=0.2, max_tokens=5)
             print(response)
 
         # If the response is false, ask GPT to fix the section for better continuity and flow
@@ -234,16 +310,81 @@ def refine_draft(book_sections, title):
     first_section = refined_sections[0]
     last_section = refined_sections[-1]
 
-    messages.append({"role": "user", "content": continuity_check_prompt(first_section, last_section)})
-    response = generate_response(messages, temperature=0.2, max_tokens=10)
+    messages.append({"role": "user", "content": continuity_check_prompt_first(first_section, last_section)})
+    response = generate_response(messages, temperature=0.2, max_tokens=5)
 
     while response.lower() not in ('true', 'false', 'true.', 'false.'):
-        messages.append({"role": "user", "content": "Please respond with only 'true' or 'false'."})
-        response = generate_response(messages, temperature=0.2, max_tokens=10)
-
+        messages.append({"role": "user", "content": "Please respond with only one word, 'true' or 'false'."})
+        response = generate_response(messages, temperature=0.2, max_tokens=5)
+        print(response)
     if response.lower() == 'false':
         return "Got lost"
     else:
-        return "\n\n".join([title,] + refined_sections)
+        return "\n\n".join(refined_sections)
+    
+
+def generate_photo(outline, title):
+    
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "You are my Photo Prompt Creator.  You take book outlines, and create a one sentence prompt to be used by openai.Image.create().  The prompt will describe a picture of a scene from the book, in a cartoon/animated style.  A cartoon picture you might use on the cover of the children's book.  Have it match the tone and feeling of the outline."},
+        {"role": "assistant", "content": "Great! Please provide me with the book outline and any specific instructions or preferences you have for the tone and feeling of the prompt."},
+        {"role": "user", "content": outline}
+         ]
+    
+    picture_prompt = generate_response(messages, temperature=0.2, max_tokens=150)
+    picture_prompt = enhance_prompt(picture_prompt)
+    
+    response = openai.Image.create(
+            prompt=picture_prompt,
+            n=1,
+            size="512x512"
+        )
+    image_url = response['data'][0]['url']
+
+    response = requests.get(image_url)
+    
+    image_file = f"{title.replace(' ', '_')}_image.png"
+    img = PIL_Image.open(BytesIO(response.content))
+    img.save(image_file, "PNG")
+    return picture_prompt, image_file
 
 
+def create_finished_copy(image_path, title, story_text, output_path):
+    doc = SimpleDocTemplate(output_path, pagesize=letter)
+
+    # Create a list to store the flowables (elements) in the document
+    elements = []
+
+    # Add the image
+    image = Image(image_path)
+    image.drawHeight = 200
+    image.drawWidth = 200
+    image.hAlign = 'CENTER'
+    elements.append(image)
+
+    # Add some space
+    elements.append(Spacer(1, 12))
+
+    # Add the title
+    styles = getSampleStyleSheet()
+    title_style = ParagraphStyle(name='TitleStyle', parent=styles['Title'], fontSize=24, alignment=TA_CENTER)
+    title_paragraph = Paragraph(title, style=title_style)
+    elements.append(title_paragraph)
+
+    # Add some space
+    elements.append(Spacer(1, 12))
+
+    # Add the story
+    story_style = ParagraphStyle(name='StoryStyle', parent=styles['Normal'], fontSize=14)
+    story_paragraphs = story_text.split('\n')
+    for paragraph in story_paragraphs:
+        elements.append(Paragraph(paragraph, style=story_style))
+        elements.append(Spacer(1, 12))
+
+    # Build the document
+    doc.build(elements)
+
+
+# Example usage:
+# create_finished_copy('path/to/image.jpg', 'Story Title', 'Story content', 'output.pdf')
--- radventure/BookBuilderTest.py
+++ radventure/BookBuilderTest.py
@@ -7,36 +7,97 @@ Created on Sat Mar 25 23:34:21 2023
 
 from BookBuilderHelpers import *
 import os
+import concurrent.futures
+import backoff
+
+def generate_photo_with_backoff(*args, **kwargs):
+    @backoff.on_exception(backoff.expo, Exception, max_tries=5)
+    def wrapped_generate_photo():
+        return generate_photo(*args, **kwargs)
+    return wrapped_generate_photo()
+
+def refine_draft_with_backoff(*args, **kwargs):
+    @backoff.on_exception(backoff.expo, Exception, max_tries=5)
+    def wrapped_refine_draft():
+        return refine_draft(*args, **kwargs)
+    return wrapped_refine_draft()
 
 def main():
-    print("Welcome to Radventure, where anything is possible!\n")
-    print("What kind of story would you like to build?")
-
-    genre = input("Enter the genre of your story: ")
-    target_audience = input("Enter the target audience age: ")
-    theme = input("Enter the theme of your story: ")
-    content_hint = input("Enter some additional details for your story: ")
+# =============================================================================
+#     print("Welcome to Radventure, where anything is possible!\n")
+#     print("What kind of story would you like to build?")
+# 
+#     genre = input("Enter the genre of your story: ")
+#     target_audience = input("Enter the target audience age: ")
+#     theme = input("Enter the theme of your story: ")
+#     setting = input("Enter a setting for your story: ")
+#     content_hint = input("Enter some additional details for your story: ")
+#     twisted = False
+#     
+#     while True:
+#         
+#         twist_response = input("Do you want a twist in the story? (yes/no): ")
+#     
+#         if twist_response.lower() in ['yes', 'no']:
+#             if twist_response == 'yes':
+#                 twisted = True
+#                 break
+#             else:
+#                 break
+#         
+#         else:
+#             print("Please respond with yes or no.")
+# =============================================================================
+            
+    genre = "uplifting"
+    target_audience = "5 year olds"
+    theme = "The importance of getting enough protein in your diet"
+    setting = "a forest"
+    content_hint = "The main characters are children who live in a cottage in the forest, Billy, Jonny, Sara and Beth."
+    twisted = False
+        
 
     print("\nCreating a prompt based on your ideas...")
-    prompt = build_prompt(genre, target_audience, theme, content_hint)
+    prompt = build_prompt(genre, target_audience, theme, setting, content_hint, twist=twisted)
 
-    enhanced = enhance_prompt(prompt)
+    enhanced = enhance_build_prompt(prompt)
+    print(enhanced)
 
     print("Coming up with cool, different plot ideas, and picking the best one...")
     outline, title = select_best_outline(enhanced)
+    print(outline)
 
     print("Writing your story...")
     sections = build_first_draft(outline)
+    if len(sections) == 0:
+        print("Oops, got a little distracted, NOW I'm writing your story...")
+        sections = build_first_draft(outline)
+        
+    print("Trimming the story down to make it less repetitive...")
+        
+    concise = process_story_sections(sections)
 
-    print("Checking and editing your story so it makes sense...")
-    refined = refine_draft(sections, title)
+    print("Checking your story so it makes sense...")
 
-    # Save the story to a txt file
-    file_name = f"{title.replace(' ', '_')}.txt"
-    with open(file_name, 'w', encoding='utf-8') as f:
-        f.write(refined)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        photo_future = executor.submit(generate_photo_with_backoff, outline, title)
+        refined_future = executor.submit(refine_draft_with_backoff, sections, title)
 
-    print(f"\nYour story '{title}' has been created and saved as '{file_name}'. Enjoy reading!")
+        description, photo_file = photo_future.result()
+        refined_draft = refined_future.result()
+    
+    print("Printing your story...")
+    output_path = f"{title.replace(' ', '_')}_storybook.pdf"
+    create_finished_copy(photo_file, title, refined_draft, output_path)
+
+    print(f"\nYour story '{title}' has been created and saved as '{output_path}'. Enjoy reading!")
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
